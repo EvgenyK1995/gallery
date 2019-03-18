8b76b24145437518fa44800505ff186a79d6d071
EvgenyK1995 <you@example.com>
2019-03-18 18:06:08 +0500

    Shit commit

--- main.js
+++ main.js
@@ -3,29 +3,13 @@ import $ from 'jquery';
 import jqui from 'jqueryui';
 
 $(document).ready(function() {
-	let drag = $('.draggable');
-	const dragW = drag.width();
-	const dragH = drag.height();
-	let drpbl = $('.droppable');
+	let main = $('.main-image');
+	let otherImages = $('.other-images');
 
 	$(function() {
-		drag.draggable({
-			containment: 'parent',
-			snap: '.droppable',
-			snapMode: 'inner'
-		});
-
-		drpbl.droppable({
-			drop: function () {
-				drag.text('in Droppable');
-				drag.width(drpbl.width()).height(drpbl.height());
-				//console.log();
-				//drag.offset({ top: drpbl.offsetTop, left: drpbl.offsetLeft });
-			},
-			out: function () {
-				drag.text('Drag me');
-				drag.width(dragW).height(dragH);
-			}
+		main.sortable();
+		otherImages.sortable({
+			connectWith: main
 		});
 	});
 });
@@ -36,7 +20,6 @@ $('.uploader__btn').change(function() {
 	for (let i = 0; i < len; i++) {
 		renderImage(this.files[i], i);
 	}
-
 });
 
 function renderImage(file, index) {
@@ -46,9 +29,9 @@ function renderImage(file, index) {
 		let theUrl = event.target.result;
 
 		if (0 === index) {
-			$('.main-image').html('<img src="' + theUrl + '" class="img-' + index + ' main-image__img" alt="mainImg" />');
+			$('.main-image').append('<img src="' + theUrl + '" class="img-' + index + ' main-image__img" alt="mainImg" />');
 		} else {
-			$('.other-images').append('<img src="' + theUrl + '" class="img-' + index + ' other-images__img" alt="otherImg" />');
+			$('.other-images').append('<div class="draggable"><img src="' + theUrl + '" class="img-' + index + ' other-images__img" alt="otherImg" /></div>');
 		}
 	};
 	reader.readAsDataURL(file);
